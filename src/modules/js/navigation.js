fc6121d98fc8ba4a47cd6fbc0da78dc7b649931e
Denis Sitnikov <specden990@mail.ru>
2024-01-02 11:07:37 +0500

    Removed console.log

--- src/modules/js/navigation.js
+++ src/modules/js/navigation.js
@@ -1,10 +1,8 @@
 export default function showingTheMenu(){
     const menuNav = document.querySelector('.header__links'),
           mobMenuBtn = document.querySelector('.header__menu-mob');
-    console.log(mobMenuBtn);
     mobMenuBtn.addEventListener('click', () =>{
         menuNav.classList.toggle('hide');
-        console.log('s');
     });
 }
 
